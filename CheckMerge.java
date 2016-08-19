de1929ee10ca5ca9a1040c34a153a358c4171580
1testgithub1 <ray200881@yahoo.com>
2016-08-18 21:37:16 -0400

    Create CheckMerge.java

---
+++ CheckMerge.java
@@ -0,0 +1,9 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class CheckMerge {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        System.out.println(names);
+    }
+}
